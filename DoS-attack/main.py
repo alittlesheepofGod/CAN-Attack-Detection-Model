8b678d5bab50a986cf78d0dd3f9d5ccb9f855863
alittlesheepofGod <huynhminhchau.k14@gmail.com>
2022-07-21 13:24:19 +0900

    ...

--- DoS-attack/main.py
+++ DoS-attack/main.py
@@ -1,6 +1,7 @@
 import libs
 import pandas as pd
 import model_lib
+from pathlib import Path
 
 # variable 
 PATH_TO_DATASET = '/mnt/d/project/dataset/CAN/Car-Hacking/DoS_dataset.csv'
@@ -13,7 +14,20 @@ X_train, X_test, y_train, y_test = libs.traintest_split(data, label)
 X_train_decimal = libs.convert_hex_decimal(X_train)
 X_test_decimal = libs.convert_hex_decimal(X_test)
 
-# model DNN 
+# output X_train, X_test, y_train, y_test to csv file 
+filepath_train = Path('./out/train.csv')
+filepath_train.parent.mkdir(parents=True, exist_ok=True)
+X_train.to_csv(filepath_train)
 
+filepath_test = Path('./out/test.csv')
+filepath_test.parent.mkdir(parents=True, exist_ok=True)
+X_test.to_csv(filepath_test)
 
+filepath_train_label = Path('./out/train_label.csv')
+filepath_train_label.parent.mkdir(parents=True, exist_ok=True)
+X_train.to_csv(filepath_train_label)
+
+filepath_test_label = Path('./out/test_label.csv')
+filepath_test_label.parent.mkdir(parents=True, exist_ok=True)
+X_test.to_csv(filepath_test_label)
 
---
+++ DoS-attack/model_lib.py
@@ -0,0 +1,16 @@
+import dnn_model
+import cnn_model
+import rnn_model
+import rbm_model
+import dbn_model
+import dbm_model
+import da_model
+
+
+
+
+
+
+
+
+
